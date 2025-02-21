c10bac047265ba035af12665699a4799c882ab47
Jackyalvarez1203 <jackyalvarez1203@gmail.com>
2025-02-20 20:42:39 -0500

    ELN

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+function toggleContent(button) {
+    var content = button.previousElementSibling.querySelector('.more-content');
+    if (content.style.display === "none") {
+        content.style.display = "block"; // Muestra el contenido
+        button.textContent = "Leer menos"; // Cambia el texto a "Leer menos"
+    } else {
+        content.style.display = "none"; // Oculta el contenido
+        button.textContent = "Leer más"; // Cambia el texto a "Leer más"
+    }
+}
