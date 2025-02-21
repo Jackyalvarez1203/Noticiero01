561f968e9c164a547946f708d42a28275e280c44
Jackyalvarez1203 <jackyalvarez1203@gmail.com>
2025-02-20 21:44:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,16 @@ function toggleMenu() {
     navLinks.classList.toggle('active');
 }
 function toggleContent(button) {
-    // Obtener el contenedor de contenido completo
-    var moreContent = button.closest('.news-item').querySelector('.more-content');
+    let moreContent;
+
+    // Verificar si el botón está dentro de la noticia principal o de una noticia secundaria
+    if (button.closest('article.featured')) {
+        // Caso para la noticia principal: Buscar .more-content dentro del contenedor principal
+        moreContent = button.closest('article').querySelector('.more-content');
+    } else {
+        // Caso para las otras noticias: Buscar .more-content dentro de .news-item
+        moreContent = button.closest('.news-item').querySelector('.more-content');
+    }
 
     // Comprobar si el contenido está oculto
     if (moreContent.style.display === "none" || moreContent.style.display === "") {
